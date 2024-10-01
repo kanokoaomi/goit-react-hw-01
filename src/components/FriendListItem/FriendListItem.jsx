a2379e0ec1bf488260e33c8117c345447f215d02
Yelyzaveta Skorohliad <skoroglyadliza@gmail.com>
2024-10-01 21:49:29 +0200

    Update FriendListItem.jsx

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -3,14 +3,12 @@ import styles from './FriendListItem.module.css'
 const FriendListItem = (props) => {
   const { avatar, name, isOnline } = props;
   return (
-    <li>
       <div className={styles.item}>
         <img className={styles.image} src={avatar} alt="Avatar" width="48" />
         <p className={styles.name}>{name}</p>
         <p className={`${isOnline ? styles.online : styles.offline}`}>
           {isOnline ? 'Online' : 'Offline'}</p>
       </div>
-    </li>
   )
 }
 
